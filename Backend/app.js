ca0d7ddd11c96f5b48728b4b15f07f4d357ef34b
Hanzlla Soomro <soomrohanzlla@gmail.com>
2025-03-08 16:16:09 +0500

    User Side complete

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,7 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const cookieParser = require('cookie-parser');
 const connectToDatabase = require('./Database/db');
 const userRoutes = require('./routes/user.routes');
 
@@ -12,6 +13,7 @@ connectToDatabase();
 app.use(cors()); // to accept requests from any origin
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.send('Hanzlla Soomro');
