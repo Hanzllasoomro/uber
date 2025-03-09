6f1c10b1c0eec9436ee015ed2c77b33d4b366cc3
Hanzlla Soomro <soomrohanzlla@gmail.com>
2025-03-10 00:47:35 +0500

    Frontend Setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import UserSignup from './Pages/UserSignup'
+import UserLogin from './Pages/UserLogin'
+import CaptainSignup from './Pages/CaptainSignup'
+import CaptainLogin from './Pages/CaptainLogin'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/user-signup" element={<UserSignup />} />
+      <Route path="/user-login" element={<UserLogin />} />
+      <Route path="/captain-signup" element={<CaptainSignup />} />
+      <Route path="/captain-login" element={<CaptainLogin />} />
+    </Routes>
   )
 }
 
